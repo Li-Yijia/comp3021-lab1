3ab3896b49e1c706a2d8a82b6f17cc6f26601bab
Li-Yijia <ylikh@connect.ust.hk>
2023-02-18 16:46:29 +0800

    HelloWorld

--- src/main/java/hk/ust/cse/comp3021/lab1/HelloWorld.java
+++ src/main/java/hk/ust/cse/comp3021/lab1/HelloWorld.java
@@ -1,13 +1,13 @@
 package hk.ust.cse.comp3021.lab1;
 
-public class Main {
+public class HelloWorld {
 
     public static int add(int a, int b) {
         return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.print("This is a demo project.");
+        System.out.print("HelloWorld!");
     }
 }
 
